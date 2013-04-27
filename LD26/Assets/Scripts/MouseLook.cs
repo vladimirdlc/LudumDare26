450df02d39bc783423058d646e9e2ef8fad832ef
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-04-27 11:22:08 -0800

    Starting mouseLook script

--- LD26/Assets/Scripts/MouseLook.cs
+++ LD26/Assets/Scripts/MouseLook.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 using System.Collections;
 
 public class MouseLook : MonoBehaviour {
-
+    Vector2 baseDirection, currentMouse, smoothMouse;
+	public float damping = 0.3f;
+		
 	// Use this for initialization
 	void Start () {
-	
+		baseDirection = transform.rotation.eulerAngles;
 	}
+	
+	void OnMouseDown() {
+        Screen.lockCursor = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+		smoothMouse = Vector2.Lerp(smoothMouse, new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), damping); 
+		currentMouse += smoothMouse;
 	}
 }
