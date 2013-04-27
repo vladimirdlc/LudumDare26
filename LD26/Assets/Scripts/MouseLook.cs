b24409e97f0701b98546237848cc98198805122a
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-04-27 01:14:25 -0800

    Moving in all directions head

--- LD26/Assets/Scripts/MouseLook.cs
+++ LD26/Assets/Scripts/MouseLook.cs
@@ -5,7 +5,6 @@ public class MouseLook : MonoBehaviour {
     Vector2 baseDirection, currentMouse, smoothMouse;
 	public float damping = 0.3f;
 		
-	// Use this for initialization
 	void Start () {
 		baseDirection = transform.rotation.eulerAngles;
 	}
@@ -14,11 +13,11 @@ public class MouseLook : MonoBehaviour {
         Screen.lockCursor = true;
     }
 	
-	// Update is called once per frame
 	void Update () {
 		smoothMouse = Vector2.Lerp(smoothMouse, new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), damping); 
 		currentMouse += smoothMouse;
 		
 		transform.localRotation = Quaternion.AngleAxis(-currentMouse.y, Quaternion.Euler(baseDirection)*Vector3.right);
+		transform.localRotation *= Quaternion.AngleAxis(currentMouse.x, transform.InverseTransformDirection(Vector3.up));
 	}
 }
