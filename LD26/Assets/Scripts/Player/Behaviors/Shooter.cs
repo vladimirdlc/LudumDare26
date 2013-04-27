66e194411c941005861c0b3899026df35c5316f2
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-04-27 03:26:53 -0800

    Starting shooter script

---
+++ LD26/Assets/Scripts/Player/Behaviors/Shooter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public int fireButton = 0;
+	
+	public float fullCharge = 1000.0f;
+	public float chargeSpeed = 100;
+	public GameObject bullet;
+	public Material chargerMaterial;
+	
+	// Update is called once per frame
+	void Update () {
+		float chargeColor;
+
+	}
+}
