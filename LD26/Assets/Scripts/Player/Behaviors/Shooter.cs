34c6abfc40ea24588f99acfd7aeb0337715f83d8
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-04-27 03:45:44 -0800

    Arms get charged

--- LD26/Assets/Scripts/Player/Behaviors/Shooter.cs
+++ LD26/Assets/Scripts/Player/Behaviors/Shooter.cs
@@ -9,9 +9,34 @@ public class Shooter : MonoBehaviour {
 	public GameObject bullet;
 	public Material chargerMaterial;
 	
+	bool isCharging = false;
+	bool isShooting = false;
+	float shootPower;
+	
+	float noCharge = 0;
+	
 	// Update is called once per frame
 	void Update () {
 		float chargeColor;
-
+		
+		if(!isShooting)
+		{
+			if(Input.GetMouseButtonDown(fireButton)) isCharging = true;
+			else if(Input.GetMouseButtonDown(fireButton)) 
+			{
+				isCharging = false;
+				//shoot
+				shootPower = 0;
+				chargerMaterial.color = Color.white;
+			}
+			
+			if(isCharging)
+			{
+				shootPower += Time.deltaTime * chargeSpeed;
+				shootPower = Mathf.Clamp(shootPower, noCharge, fullCharge);
+				chargeColor = shootPower/fullCharge;
+				chargerMaterial.color = Color.Lerp(Color.white, Color.red, chargeColor);
+			}
+		}
 	}
 }
